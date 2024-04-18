ccc5cba07a2471caa953dfb3d912f858fc32c0f0
bioroid17 <trace1997@ajou.ac.kr>
2024-04-18 16:15:09 +0900

    PRACTICE MOVIE APP - Coin Tracker
    Code Challenge - How many BTC can you buy with <input> USD?

--- src/App.js
+++ src/App.js
@@ -1,35 +1,56 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 
 function App() {
-  const [toDo, setToDo] = useState("");
-  const [toDos, setToDos] = useState([]);
-  const onChange = (event) => setToDo(event.target.value);
-  const onSubmit = (event) => {
-    event.preventDefault();
-    if (toDo === "") {
-      return;
+  const [loading, setLoading] = useState(true);
+  const [coins, setCoins] = useState([]);
+  const [money, setMoney] = useState("0");
+  const [coin, setCoin] = useState(null);
+  useEffect(() => {
+    fetch("https://api.coinpaprika.com/v1/tickers")
+      .then((response) => response.json())
+      .then((json) => {
+        setCoins(json);
+        setLoading(false);
+      });
+  }, []);
+  useEffect(() => {
+    if (coins.length !== 0) {
+      setCoin(coins[0]);
     }
-    setToDos((currentArray) => [toDo, ...currentArray]);
-    setToDo("");
-  };
+  }, [coins]);
+  const onChange = (event) => setMoney(event.target.value);
+  const onSelect = (event) => setCoin(JSON.parse(event.target.value));
   return (
     <div>
-      <h1>My To Dos ({toDos.length})</h1>
-      <form onSubmit={onSubmit}>
-        <input
-          value={toDo}
-          onChange={onChange}
-          type="text"
-          placeholder="Write your to do..."
-        />
-        <button>Add To Do</button>
-      </form>
-      <hr />
-      <ul>
-        {toDos.map((item, index) => (
-          <li key={index}>{item}</li>
-        ))}
-      </ul>
+      <h1>The Coins! {loading ? "" : `(${coins.length})`}</h1>
+      {loading ? (
+        <strong>Loading...</strong>
+      ) : (
+        <select onChange={onSelect}>
+          {coins.map((coin) => (
+            <option key={coin.id} value={JSON.stringify(coin)}>
+              {coin.name} ({coin.symbol}): ${coin.quotes.USD.price} USD
+            </option>
+          ))}
+        </select>
+      )}
+      <br />
+      <input value={money} onChange={onChange} type="number" /> USD
+      <br />
+      <br />
+      is equals to...
+      {loading ? (
+        ""
+      ) : (
+        <h2>
+          {money.length === 0
+            ? 0
+            : coin != null
+            ? parseFloat(money) / coin.quotes.USD.price
+            : 0}
+          &nbsp;BTC {coin != null ? `(${coin.symbol})` : null}
+        </h2>
+      )}
     </div>
   );
 }
