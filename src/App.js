359dae8ed28a061dc2bb8d36f6e1de474bf1350c
bioroid17 <trace1997@ajou.ac.kr>
2024-04-18 18:15:32 +0900

    PRACTICE MOVIE APP - Movie App part 1

--- src/App.js
+++ src/App.js
@@ -2,54 +2,39 @@ import { useEffect, useState } from "react";
 
 function App() {
   const [loading, setLoading] = useState(true);
-  const [coins, setCoins] = useState([]);
-  const [money, setMoney] = useState("0");
-  const [coin, setCoin] = useState(null);
+  const [movies, setMovies] = useState([]);
+  const getMovies = async () => {
+    const response = await fetch(
+      "https://yts.mx/api/v2/list_movies.json?minimum_rating=8.5&sort_by=year"
+    );
+    const json = await response.json();
+    setMovies(json.data.movies);
+    setLoading(false);
+  };
   useEffect(() => {
-    fetch("https://api.coinpaprika.com/v1/tickers")
-      .then((response) => response.json())
-      .then((json) => {
-        setCoins(json);
-        setLoading(false);
-      });
+    getMovies();
   }, []);
-  useEffect(() => {
-    if (coins.length !== 0) {
-      setCoin(coins[0]);
-    }
-  }, [coins]);
-  const onChange = (event) => setMoney(event.target.value);
-  const onSelect = (event) => setCoin(JSON.parse(event.target.value));
   return (
     <div>
-      <h1>The Coins! {loading ? "" : `(${coins.length})`}</h1>
       {loading ? (
-        <strong>Loading...</strong>
+        <h1>Loading...</h1>
       ) : (
-        <select onChange={onSelect}>
-          {coins.map((coin) => (
-            <option key={coin.id} value={JSON.stringify(coin)}>
-              {coin.name} ({coin.symbol}): ${coin.quotes.USD.price} USD
-            </option>
+        <div>
+          {movies.map((movie) => (
+            <div key={movie.id}>
+              <img src={movie.medium_cover_image} />
+              <h2>{movie.title}</h2>
+              <p>{movie.summary}</p>
+              {movie.hasOwnProperty("genres") ? (
+                <ul>
+                  {movie.genres.map((genre) => (
+                    <li key={genre}>{genre}</li>
+                  ))}
+                </ul>
+              ) : null}
+            </div>
           ))}
-        </select>
-      )}
-      <br />
-      <input value={money} onChange={onChange} type="number" /> USD
-      <br />
-      <br />
-      is equals to...
-      {loading ? (
-        ""
-      ) : (
-        <h2>
-          {money.length === 0
-            ? 0
-            : coin != null
-            ? parseFloat(money) / coin.quotes.USD.price
-            : 0}
-          &nbsp;BTC {coin != null ? `(${coin.symbol})` : null}
-        </h2>
+        </div>
       )}
     </div>
   );
