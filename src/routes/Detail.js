87c2f46bd68d2f17e0b184fd39f413924ddacf1c
bioroid17 <trace1997@ajou.ac.kr>
2024-04-21 15:20:57 +0900

    PRACTICE MOVIE APP - Parameters
    Code Challenge - Movie Details

--- src/routes/Detail.js
+++ src/routes/Detail.js
@@ -1,4 +1,39 @@
+import { useEffect, useState } from "react";
+import { useParams } from "react-router-dom/cjs/react-router-dom.min";
+
 function Detail() {
-  return <h1>Detail</h1>;
+  const { id } = useParams();
+  const [loading, setLoading] = useState(true);
+  const [movie, setMovie] = useState(null);
+  const getMovie = async () => {
+    const json = await (
+      await fetch(`https://yts.mx/api/v2/movie_details.json?movie_id=${id}`)
+    ).json();
+    setMovie(json.data.movie);
+    setLoading(false);
+  };
+  useEffect(() => {
+    getMovie();
+  });
+  return (
+    <div>
+      {loading ? (
+        <h1>Loading...</h1>
+      ) : (
+        <div>
+          <h1>{movie.title}</h1>
+          <img src={movie.large_cover_image} alt={movie.title} />
+          <h4>Rating: {movie.rating}</h4>
+          <ul>
+            {movie.genres
+              ? movie.genres.map((genre, index) => <li key={index}>{genre}</li>)
+              : null}
+          </ul>
+          <hr />
+          <p>{movie.description_full}</p>
+        </div>
+      )}
+    </div>
+  );
 }
 export default Detail;
